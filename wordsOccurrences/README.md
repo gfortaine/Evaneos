3e60436fb749a0d2fd7c5435344aa4668c3b7895
Guillaume FORTAINE <guillaume@quocian.com>
2017-02-09 14:14:56 +0100

    initial commit

---
+++ wordsOccurrences/README.md
@@ -0,0 +1,2 @@
+On cherche à calculer le nombre d'occurence de chaque mots dans un texte. Les mots de moins de deux lettres seront ignorés. 
+Concevez une petite librairie JS qui répond à ce problème. Dans le doute, utilisez votre bon sens.
---
+++ wordsOccurrences/wordsOccurrences.js
@@ -0,0 +1,14 @@
+const wordOccurrences = (text) => {
+  const occurrences = {}
+  const regEx = new RegExp('[A-z-À-ÿ]+', 'g')
+  
+  text.match(regEx).reduce((prev, curr) => {
+    const word = `${curr.slice(0, 1).toLowerCase()}${curr.slice(1)}`
+    
+    return (word.length > 1)
+           ? occurrences[word] = ((occurrences[word] += 1) || 1)
+           : null
+    }, [])
+    
+  return occurrences  
+}    
