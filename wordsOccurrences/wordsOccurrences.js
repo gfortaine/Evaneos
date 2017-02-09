2350ce661ee233c358c490726361119d14922bff
Guillaume FORTAINE <guillaume@quocian.com>
2017-02-09 19:57:59 +0100

    Better reduce usage

--- wordsOccurrences/wordsOccurrences.js
+++ wordsOccurrences/wordsOccurrences.js
@@ -1,14 +1,11 @@
 const wordOccurrences = (text) => {
-  const occurrences = {}
   const regEx = new RegExp('[-0-9a-zà-ÿ]+', 'gi')
   
-  text.match(regEx).reduce((prev, curr) => {
-    const word = `${curr.toLowerCase()}`
+  return text.match(regEx).reduce((prev, curr) => {
+    const word = curr.toLowerCase()
     
     return (word.length > 1)
-           ? occurrences[word] = ((occurrences[word] += 1) || 1)
-           : null
-    }, [])
-    
-  return occurrences  
+           ? (prev[word] = ((prev[word] += 1) || 1), prev)
+           : prev
+    }, {})    
 }    
