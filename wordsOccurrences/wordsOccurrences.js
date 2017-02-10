6d33b136c47a451d4a744a155d36638ca1214ab5
Guillaume FORTAINE <guillaume@quocian.com>
2017-02-10 16:31:59 +0100

    Code optimization

--- wordsOccurrences/wordsOccurrences.js
+++ wordsOccurrences/wordsOccurrences.js
@@ -1,7 +1,7 @@
 const wordsOccurrences = (text) => {
   const regEx = new RegExp('[-0-9a-zà-ÿ]+', 'gi')
   
-  return text.match(regEx).reduce((prev, curr) => {
+  return (text.match(regEx) || []).reduce((prev, curr) => {
     const word = curr.toLowerCase()
     
     return (((word.length > 1) ? prev[word] = (++prev[word] || 1) : null), prev) }, Object.create(null))
