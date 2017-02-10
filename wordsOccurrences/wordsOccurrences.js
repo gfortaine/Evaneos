ccc2f9e74a79c12a954468d7f9a5d7a112b7b14a
Guillaume FORTAINE <guillaume@quocian.com>
2017-02-10 01:56:46 +0100

    Code optimization

--- wordsOccurrences/wordsOccurrences.js
+++ wordsOccurrences/wordsOccurrences.js
@@ -4,5 +4,5 @@ const wordOccurrences = (text) => {
   return text.match(regEx).reduce((prev, curr) => {
     const word = curr.toLowerCase()
     
-    return (((word.length > 1) ? prev[word] = ((prev[word] += 1) || 1) : null), prev) }, {})
+    return (((word.length > 1) ? prev[word] = (++prev[word] || 1) : null), prev) }, {})
 }    
