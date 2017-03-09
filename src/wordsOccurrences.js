5c9c24e5594407f6d55f1b83f40078e7133711cc
Guillaume FORTAINE <guillaume@quocian.com>
2017-03-09 08:30:34 +0100

    fixes

--- src/wordsOccurrences.js
+++ src/wordsOccurrences.js
@@ -1,10 +1,12 @@
 // @flow
 
-type MapOfNumbers = {[key: string]: ?number};
+type MapOfNumbers = { [key: string]: ?number };
 
-export default (text: string): MapOfNumbers =>
-  (text.toLowerCase().match(/[-0-9a-zà-ÿ]+/g) || []).reduce(
-    (prev, curr) =>
-      curr.length > 1 ? { __proto__: null, ...prev, [curr]: prev[curr] + 1 || 1 } : prev,
-    { __proto__: null },
-  )
+export default (text: string): MapOfNumbers => (text.toLowerCase().match(/[-0-9a-zà-ÿ]+/g) || [
+]).reduce(
+  (prev, curr) =>
+    curr.length > 1 ? { __proto__: null, ...prev, [curr]: prev[curr] + 1 || 1 } : prev,
+  {
+    __proto__: null,
+  },
+);
