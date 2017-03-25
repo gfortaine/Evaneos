a6ca13f2028a389cf38315c4c988964db1725a42
Guillaume FORTAINE <guillaume@quocian.com>
2017-03-25 12:43:30 +0100

    ncu -a

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-On cherche à calculer le nombre d'occurences de chaque mots dans un texte. Les mots de moins de deux lettres seront ignorés. 
+On cherche à calculer le nombre d'occurences de chaque mot dans un texte. Les mots de moins de deux lettres seront ignorés. 
 Concevez une petite librairie JS qui répond à ce problème. Dans le doute, utilisez votre bon sens.
--- src/wordsOccurrences.js
+++ src/wordsOccurrences.js
@@ -2,11 +2,12 @@
 
 type MapOfNumbers = { [key: string]: ?number };
 
-export default (text: string): MapOfNumbers => (text.toLowerCase().match(/[-0-9a-zà-ÿ]+/g) || [
-]).reduce(
-  (prev, curr) =>
-    curr.length > 1 ? { __proto__: null, ...prev, [curr]: prev[curr] + 1 || 1 } : prev,
-  {
-    __proto__: null,
-  },
-);
+export default (text: string): MapOfNumbers =>
+  (text.toLowerCase().match(/[-0-9a-zà-ÿ]+/g) || [])
+    .reduce(
+      (prev, curr) =>
+        curr.length > 1 ? { __proto__: null, ...prev, [curr]: prev[curr] + 1 || 1 } : prev,
+    {
+      __proto__: null,
+    },
+    );
